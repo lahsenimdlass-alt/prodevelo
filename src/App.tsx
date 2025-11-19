abb2dad3cc4bb71da96bb6886543ae3da70f35d2
lahsenimdlass-alt <lahsen.imdlass@gmail.com>
2025-11-20 00:38:43 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import ContactPage from './pages/ContactPage';
+import LandingPage from './pages/LandingPage';
 
 function App() {
+  const location = useLocation();
+  const isLandingPage = location.pathname === '/demo';
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      {!isLandingPage && <Header />}
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/services" element={<ServicesPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/demo" element={<LandingPage />} />
         </Routes>
       </main>
-      <Footer />
-      <WhatsAppButton />
+      {!isLandingPage && <Footer />}
+      {!isLandingPage && <WhatsAppButton />}
     </div>
   );
 }
